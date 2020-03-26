f8768abdeb44eed2e17eb5e622e8031713b501c3
Sieg-Alejandro <sieg.alejandro@gmail.com>
2020-03-26 16:46:41 -0400

    Init commit

---
+++ README.txt
@@ -0,0 +1,17 @@
+0. Chase O'Donnell- cco49 ////Sieg Alejandro-ska89
+
+1. our client is recursive because for every query We will wait until we have talked to every sir very necessary before completing the query
+When we get an NS from the route server we automatically go straight to the TS before writing a response for the queryif the resolved text fileIs 
+thought of as the end-user then we see that it never gets an NS record
+rather our client program will recursively handle the record until we either get an a record or not found
+
+2. There are no known issues with the attached code
+
+3. One problem we had developing this code was that after all the lines had been proccessed by DNS server
+there would always be an extre new line character that we needed to get rid of. I found it difficult editing 
+the file with python so i resorted to running unix within our client python program to edit the file
+
+
+4. We gained a comprehensive understanding of how a DNS works by creating this scaled down version of One
+
+
---
+++ rs.py
@@ -0,0 +1,74 @@
+import threading
+import time
+import random
+import socket as mysoc
+import pickle
+import sys
+# server task
+def server():
+    if len(sys.argv) != 2:
+        print "Improper arguments. Please enter socket number"
+        exit()
+    if  not sys.argv[1].isdigit():
+        print "Please enter a valid socket number"
+        exit()
+    
+    portnum=int(sys.argv[1])
+
+    try:
+        ss=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+        ss.setsockopt(mysoc.SOL_SOCKET,mysoc.SO_REUSEADDR,1)
+        print("[S]: Server socket created")
+    except mysoc.error as err:
+        print('{} \n'.format("socket open error ",err))
+    server_binding=('',portnum)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host=mysoc.gethostname()
+    print("[S]: Server host name is: ",host)
+    localhost_ip=(mysoc.gethostbyname(host))
+    print("[S]: Server IP address is  ",localhost_ip)
+
+    #Open File
+    fp=open('PROJI-DNSRS.txt')
+    lines=fp.readlines()
+    DNSTABLE={}
+    TSHostname=""
+    for line in lines:
+        print(line)
+        (hostname,ip,rtype)=line.split()
+        if rtype=="NS":
+            TSHostname=hostname
+        DNSTABLE[hostname.lower()]=(ip,rtype)
+
+    print DNSTABLE
+
+    csockid,addr=ss.accept()
+    print ("[S]: Got a connection request from a client at", addr)
+    # send a intro  message to the client.
+    while(True):
+        client_recv=csockid.recv(4096)
+        client_query=client_recv.decode('utf-8').rstrip()
+        if(client_query=="finished sending"):
+            break
+        server_response=""
+        print("client queery: "+ client_query.lower()+'\n')
+        if client_query.lower() in DNSTABLE:
+            ip,rtype=DNSTABLE[client_query.lower()]
+            hostname=client_query
+            server_response="%s %s %s" % (client_query,ip,rtype)
+            print server_response
+            csockid.send(server_response.encode('utf-8'))
+            continue
+        if client_query or client_query!="":
+            server_response="%s - NS" % TSHostname
+            csockid.send(server_response.encode('utf-8'))
+       
+        
+        
+   # Close the server socket
+    ss.close()
+    exit()
+
+t1 = threading.Thread(name='server', target=server)
+t1.start()
